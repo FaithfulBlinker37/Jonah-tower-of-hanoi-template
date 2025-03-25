7c5014bab20ec9a6733f22dbce88bc029ae87f9f
FaithfulBlinker37 <jonahwang26@gmail.com>
2025-03-25 20:28:11 +0000

    commit 10

--- TowerSolver.java
+++ TowerSolver.java
@@ -19,5 +19,10 @@ public class TowerSolver {
     //
     // [ solve method here]
     //
+    private void solve(int n, int source, int destination, int auxiliary) {
+        if (n == 1) {
+            model.move(source, destination);
+            return;
+        }
 
 }
